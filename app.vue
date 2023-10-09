05f4dfc9e3b9ef499904127913bf8f209451738e
Liza <lizavetayalak2000@gmail.com>
2023-10-09 21:23:04 +0200

    feat: add table, fetch data

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <UContainer>
+      <div v-if="checkDataArr">
+        <UTable  v-if="dataArr" :rows="dataArr.posts"></UTable>
+      </div>
+    </UContainer>
 </template>
+
+<script setup lang="ts">
+
+  import { onMounted } from "vue";
+
+  let dataArr;
+
+  onMounted(async () => {
+    const data = await fetch('https://dummyjson.com/posts')
+    dataArr = await data.json()
+
+    console.log(dataArr)
+  })
+
+  function checkDataArr(){
+    return !!dataArr;
+  }
+
+</script>
