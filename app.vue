43cdbfd94fe94295f44e39cb439eac1f96b35265
Liza <lizavetayalak2000@gmail.com>
2023-10-10 13:00:31 +0200

    feat: add pagination

--- app.vue
+++ app.vue
@@ -1,15 +1,29 @@
 <template>
-  <UContainer>
-    <UTable :rows="data.posts" :columns="columns">
+  <UContainer v-if="data">
+    <UTable
+      :rows="data.posts"
+      :columns="columns"
+      :loading="pending"
+      :loading-state="{ icon: 'i-heroicons-arrow-path-20-solid', label: 'Loading...' }"
+    >
       <template #tags-data="{ row }">
         <UBadge v-for="tag in row.tags" :key="tag" class="ml-2">{{ tag }}</UBadge>
       </template>
     </UTable>
+    <UPagination v-model="page" :page-count="pageCount" :total="data.total" />
   </UContainer>
 </template>
 
 <script setup lang="ts">
-  const { data } = await useFetch('https://dummyjson.com/posts');
+  const page = ref(1);
+  const pageCount = 10;
+
+  const { data, pending } = await useFetch('https://dummyjson.com/posts', {
+    query: {
+      limit: pageCount,
+      skip: computed(() => (page.value - 1) * pageCount),
+    }
+  });
 
   const columns = [{
     key: 'id',
